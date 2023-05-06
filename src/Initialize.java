04a7e6731717be99d67e507902d243b47b608b1f
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-06 20:49:11 +0530

    6

--- src/Initialize.java
+++ src/Initialize.java
@@ -2,5 +2,10 @@ public class Initialize {
     public static void main(String[] args) {
         AppServer legacyUi = new LegacyAppUI();
         legacyUi.displayData(new XMLData());
+        //-----
+        AppServer modernUi = new ModernUiAdapter(
+                new ModernAppUi()
+        );
+        modernUi.displayData(new XMLData());
     }
 }
